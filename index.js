345211357945b9b40067d25a9a144a11e4d74fe5
Michael Kovacic <michael.j.kovacic@gmail.com>
2018-02-15 16:05:06 -0700

    directory at baseline

---
+++ index.js
@@ -0,0 +1,6 @@
+//contains the logic for the course of the game, depends on words.js
+	//randomly selects a word and uses the word constructor to store it
+	//prompts the user for each guess and keeps track of the user's remaining guesses
+
+
+//requires word.js
---
+++ letter.js
@@ -0,0 +1,9 @@
+//need a constructor that contains
+	//string value to underlying value for the character
+	//boolean to see if it has been guessed
+	//a function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
+	//A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
+
+//i need to export to word.js
+
+
